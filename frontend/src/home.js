a2c9ceb10fdde2d7376be3bc59312128dcc18357
ranit2004 <ranitbarua2004@gmail.com>
2024-02-04 09:12:58 +0530

    name change

--- frontend/src/home.js
+++ frontend/src/home.js
@@ -221,7 +221,7 @@ export const ImageUpload = () => {
       <AppBar position="static" className={classes.appbar}>
         <Toolbar>
           <Typography className={classes.title} variant="h6" noWrap>
-            CodeBasics: Potato Disease Classification
+            leaf Disease Classification
           </Typography>
           <div className={classes.grow} />
           <Avatar src={cblogo}></Avatar>
